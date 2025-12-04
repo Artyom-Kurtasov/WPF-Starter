e8c091a50d2f430da43aef7d55e70f8173929fc8
Артем Куртасов <prvimrlol@gmail.com>
2025-12-04 21:20:11 +0300

    version 1.0.2

--- WPF Starter/Services/FileServices/CreateRootElement.cs
+++ WPF Starter/Services/FileServices/CreateRootElement.cs
@@ -14,24 +14,30 @@ namespace WPF_Starter.Services.FileServices
         /// </summary>
         public void Fill(AppDbContext dataBase, ExportSettings exportSettings, Search search, Paginator paginator, PagingSettings pagingSettings)
         {
-            XDocument? doc = XDocument.Load(exportSettings.XmlFileName);
+            XDocument doc = new XDocument(new XElement("TestProgram"));
+
+            int idCounter = 1;
 
             foreach (List<People> batch in paginator.Pagenation(dataBase, pagingSettings, search.SearchPeople(dataBase)))
             {
-                foreach (People? person in batch)
+                foreach (People person in batch)
                 {
-                    XElement personElement = new XElement("Person",
-                        new XElement("Date", $"{person.Date}"),
-                        new XElement("Name", $"{person.Name}"),
-                        new XElement("Surname", $"{person.Surname}"),
-                        new XElement("Patronymic", $"{person.Patronymic}"),
-                        new XElement("City", $"{person.City}"),
-                        new XElement("Country", $"{person.Country}"));
-                    doc.Root.Add(personElement);
+                    XElement recordElement = new XElement("Record",
+                        new XAttribute("id", idCounter++),
+                        new XElement("Date", person.Date),
+                        new XElement("FirstName", person.Name),
+                        new XElement("LastName", person.Surname),
+                        new XElement("SurName", person.Patronymic),
+                        new XElement("City", person.City),
+                        new XElement("Country", person.Country)
+                    );
+
+                    doc.Root.Add(recordElement);
                 }
             }
 
             doc.Save(exportSettings.XmlFileName);
         }
+
     }
 }
